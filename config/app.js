a5a976c814ede0591c59bf2f6024023959d789a7
MD Arwaz Jamal <arwaz.jamal@codinova.com>
2019-05-25 07:58:34 +0530

    Added new host

--- config/app.js
+++ config/app.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//   HOST: 'https://spotify-app-scp.herokuapp.com/'
+// };
+
 module.exports = {
-  HOST: 'https://spotify-app-scp.herokuapp.com/'
+  HOST: 'http://localhost:3000'
 };
--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,6 @@ import withRedux from 'next-redux-wrapper';
 
 import { initStore } from '../store/store';
 import PageWithIntl from '../components/PageWithIntl';
-
 const About = () => (
   <Layout>
     <style jsx>
@@ -11,15 +10,38 @@ const About = () => (
         p {
           margin: 20px 40px 0;
         }
+        div {
+          color: #333;
+          font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+          font-size: 15px;
+          margin: 10px;
+        }
       `}
     </style>
-
     <p>
       <a href="https://github.com/zeit/next.js">Next.js</a> and{' '}
       <a href="https://developer.spotify.com/web-api/web-api-connect-endpoint-reference/">
         Spotify's Connect API endpoints
       </a>
     </p>
+    <div>
+      <div className="feature1">
+        <h3>Full Spotify Catalogue</h3>
+        <p>Search and choose any song on Spotify</p>
+      </div>
+      <div className="feature2">
+        <h3>Voting</h3>
+        <p>Vote for songs added by other users in the room. The most voted one will play next.</p>
+      </div>
+      <div className="feature3">
+        <h3> Music Goes On</h3>
+        <p>When the queue is finished we will add similar songs automatically</p>
+      </div>
+      <div className="feature4">
+        <h3>Play With Spotify Connect</h3>
+        <p>Choose to play on your Spotify application or speaker (requires Spotify Premium).</p>
+      </div>
+    </div>
   </Layout>
 );
 
